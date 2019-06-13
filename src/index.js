9a2500942720c7b1b5e0711fab5e8aee69644c39
Eugene <eugeneburlak@yandex.ru>
2019-06-13 14:48:43 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,49 +1,5 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Form from "./export.js";
+import Form from "./Form";
 
-class App extends Component{
-  render(){
-    return (
-      <div>
-        <Form
-            onSubmit={(data) => {
-              console.log(data);
-              return new Promise((resolve, reject) => {
-                resolve();
-              })
-            }}
-            fields={[
-              {
-                name: 'radios',
-                label: 'ENTER_MESSAGE',
-                type: 'select',
-                col: 'second',
-                size: 1,
-                options: [{
-                  value: 'first',
-                  text: 'first text'
-                },{
-                  value: 'second',
-                  text: 'second text'
-                },{
-                  value: 'third',
-                  text: 'third text'
-                }],
-                validation: (array) => {
-                  if(!array.length) return 'REQUIRED';
-                }
-              },
-              {
-                value: 'SEND_MESSAGE',
-                type: 'submit',
-                col: 'second'
-              }
-            ]}
-          />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default Form;
