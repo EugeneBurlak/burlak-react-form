b3d17f0b756badfd3696ea2517b767f8ce3ff13f
Eugene <eugeneburlak@yandex.ru>
2019-07-11 17:05:06 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -5,82 +5,82 @@ class App extends Component {
   render() {
     return (
       <Form
-        styled
-        autoReset={true}
-        onSubmit={data => {
-          console.log(data);
-          return new Promise((resolve, reject) => {
-            resolve();
-          });
-        }}
-        fields={[
-          {
-            name: 'radios',
-            label: 'ENTER_MESSAGE',
-            type: 'radio',
-            value: 'first',
-            inline: true,
-            resetButton: {
-              enable: true
-            },
-            options: [
-              {
-                value: 'first',
-                text: 'first text'
+          styled
+          autoReset={true}
+          onSubmit={data => {
+            console.log(data);
+            return new Promise((resolve, reject) => {
+              resolve();
+            });
+          }}
+          fields={[
+            {
+              name: 'radios',
+              label: 'ENTER_MESSAGE',
+              type: 'radio',
+              value: 'first',
+              inline: true,
+              resetButton: {
+                enable: true
               },
-              {
-                value: 'second',
-                text: 'second text'
-              },
-              {
-                value: 'third',
-                text: 'third text'
+              options: [
+                {
+                  value: 'first',
+                  text: 'first text'
+                },
+                {
+                  value: 'second',
+                  text: 'second text'
+                },
+                {
+                  value: 'third',
+                  text: 'third text'
+                }
+              ],
+              validation: checked => {
+                if (!checked.length) return 'REQUIRED';
               }
-            ],
-            validation: checked => {
-              if (!checked.length) return 'REQUIRED';
-            }
-          },
-          {
-            name: 'select',
-            label: 'ERROR',
-            type: 'select',
-            multiple: true,
-            value: ['second2', 'first2second2'],
-            resetButton: {
-              enable: true
             },
-            options: [
-              {
-                value: 'first2second2',
-                text: 'first text2'
+            {
+              name: 'select',
+              label: 'ERROR',
+              type: 'select',
+              multiple: true,
+              value: ['second2', 'first2second2'],
+              resetButton: {
+                enable: true
               },
-              {
-                value: 'second2',
-                text: 'second text2'
-              },
-              {
-                value: 'third2',
-                text: 'third text'
+              options: [
+                {
+                  value: 'first2second2',
+                  text: 'first text2'
+                },
+                {
+                  value: 'second2',
+                  text: 'second text2'
+                },
+                {
+                  value: 'third2',
+                  text: 'third text'
+                }
+              ],
+              validation: checked => {
+                if (!checked.length) return 'REQUIRED';
               }
-            ],
-            validation: checked => {
-              if (!checked.length) return 'REQUIRED';
+            },
+            {
+              name: 'dsadadas',
+              type: 'file',
+              placeholder: '1',
+              disabled: true,
+            },
+            {
+              value: 'SEND_MESSAGE',
+              type: 'submit',
+              col: 'second'
             }
-          },
-          {
-            name: 'dsadadas',
-            type: 'file',
-            placeholder: '1',
-            disabled: true,
-          },
-          {
-            value: 'SEND_MESSAGE',
-            type: 'submit',
-            col: 'second'
-          }
-        ]}
-      />
+          ]}
+        />
     );
   }
 }
